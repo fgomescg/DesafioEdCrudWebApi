d1f4f8c9c7e16e8a8bdf8eaaa7bec08e8b45e011
Fábio Gomes <fgomescg@gmail.com>
2020-10-08 13:50:00 -0300

    PagedList agora começa sempre do zero.

--- DesafioEdCRUD/Entities/Models/PagedList.cs
+++ DesafioEdCRUD/Entities/Models/PagedList.cs
@@ -23,7 +23,7 @@ namespace Entities.Models
     public static PagedList<T> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
     {
       var count = source.Count();
-      var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+      var items = source.Skip((pageNumber) * pageSize).Take(pageSize).ToList();
 
       return new PagedList<T>(items, count, pageNumber, pageSize);
     }
